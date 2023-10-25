d9f802366f1f64bab8acfeaa3158ca754a36b0b9
Alexander Rozum <roozumaleks@gmail.com>
2023-10-25 13:22:05 +0300

    Structured Concurrency часть вторая + ExceptionHandler

--- app/src/main/java/ru/sumin/coroutinestart/MainViewModel.kt
+++ app/src/main/java/ru/sumin/coroutinestart/MainViewModel.kt
@@ -2,44 +2,55 @@ package ru.sumin.coroutinestart
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlin.concurrent.thread
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
     private val parentJob = Job()
-    private val coroutineScope = CoroutineScope(Dispatchers.IO + parentJob)
+    private val exception = CoroutineExceptionHandler { _, throwable ->
+        Log.d(
+            LOG_TAG,
+            "Исключение поймано: $throwable"
+        )
+    }
+    private val coroutineScope = CoroutineScope(Dispatchers.IO + parentJob + exception)
     fun method() {
         val childJob1 = coroutineScope.launch {
-            delay(3000)
+            delay(1000)
             Log.d(LOG_TAG, "Первая корутина завершила работу")
+            launch {
+                launch {
+                    delay(3000)
+                    error()
+                }
+                Log.d(LOG_TAG, "Четвертая корутина завершила работу")
+            }
         }
         val childJob2 = coroutineScope.launch {
-            delay(2000)
-            childJob1.cancel()
+            delay(5000)
             Log.d(LOG_TAG, "Вторая корутина завершила работу")
-            Log.d(LOG_TAG, "Родительская корутина завершила работу: ${parentJob.isCancelled}")
+            launch {
+                Log.d(LOG_TAG, "Пятая корутина завершила работу")
+            }
         }
-//        thread {
-//            Thread.sleep(1000)
-//            parentJob.cancel() // Завершил свою работу, все его наследники (корутины) были отменены
-//            Log.d(LOG_TAG, "Родительский Job активный: ${parentJob.isActive}") // Активный
-//        }
-        Log.d(LOG_TAG, parentJob.children.contains(childJob1).toString()) // true
-        Log.d(LOG_TAG, parentJob.children.contains(childJob2).toString()) // true
+        val childJob3 = coroutineScope.launch {
+            delay(3000)
+            Log.d(LOG_TAG, "Третья корутина завершила работу")
+        }
+    }
 
+    private fun error() {
+        throw RuntimeException()
     }
 
     override fun onCleared() {
         super.onCleared()
-        thread {
-            Log.d(LOG_TAG, "Родительский Job активный: ${parentJob.isActive}")
-        }
         coroutineScope.cancel()
     }
 
