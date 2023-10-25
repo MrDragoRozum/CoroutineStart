5beba79dfb93685187dbe1eca96756e6a4aae11e
Alexander Rozum <roozumaleks@gmail.com>
2023-10-25 15:47:49 +0300

    Отмена корутины

--- app/src/main/java/ru/sumin/coroutinestart/MainViewModel.kt
+++ app/src/main/java/ru/sumin/coroutinestart/MainViewModel.kt
@@ -3,54 +3,33 @@ package ru.sumin.coroutinestart
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.async
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
-    private val parentJob = SupervisorJob()
-    private val exception = CoroutineExceptionHandler { _, throwable ->
-        Log.d(
-            LOG_TAG,
-            "Исключение поймано: $throwable"
-        )
-    }
-    private val coroutineScope = CoroutineScope(Dispatchers.IO + parentJob + exception)
     fun method() {
-        val childJob1 = coroutineScope.launch {
-            delay(3000)
-            Log.d(LOG_TAG, "Первая корутина завершила работу")
-        }
-        val childJob2 = coroutineScope.launch {
-            delay(2000)
-            Log.d(LOG_TAG, "Вторая корутина завершила работу")
-        }
-        val childJob3 = coroutineScope.async {
-            launch {
-                delay(1000)
-                error()
-                Log.d(LOG_TAG, "Третья корутина завершила работу")
+        val job = viewModelScope.launch(Dispatchers.Default) {
+            Log.d(LOG_TAG, "Старт")
+            val before = System.currentTimeMillis()
+            var count = 0
+            for (i in 0 until 100_000_000) {
+                for (j in 0 until 100) {
+                    ensureActive()
+                    count++
+                }
             }
+            Log.d(LOG_TAG, "Завершился: ${System.currentTimeMillis() - before}")
         }
-        coroutineScope.launch {
-            childJob3.await()
+        job.invokeOnCompletion {
+            Log.d(LOG_TAG, "Корутина завершила свою работу. $it")
         }
-    }
-
-    private fun error() {
-        throw RuntimeException()
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        coroutineScope.cancel()
+        viewModelScope.launch {
+            delay(3000)
+            job.cancel()
+        }
     }
 
     companion object {
